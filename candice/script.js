fb413c20f696e32092bab401ad67dcd0b888af55
xingyu.lin <23546860+starUnvs@users.noreply.github.com>
2024-11-04 22:51:43 +0800

    v0.2, add background and card color

--- candice/script.js
+++ candice/script.js
@@ -1,46 +1,4 @@
-document.querySelectorAll('.card').forEach(card => {
-    const question = card.getAttribute('data-question');
-    const options = JSON.parse(card.getAttribute('data-options'));
-    const cardId = card.getAttribute('data-id');
-
-    const questionElem = card.querySelector('.question');
-    const optionsElem = card.querySelector('.options');
-    const resultsElem = card.querySelector('.results');
-
-    questionElem.textContent = question;
-
-    let voteCounts = JSON.parse(localStorage.getItem(`votes_${cardId}`)) || { A: 0, B: 0, C: 0, D: 0 };
-
-    optionsElem.innerHTML = Object.entries(options).map(([key, value]) => `
-        <button class="vote-option" data-option="${key}">${value}</button>
-    `).join('');
-
-    optionsElem.querySelectorAll('.vote-option').forEach(optionButton => {
-        optionButton.addEventListener('click', () => {
-            const option = optionButton.getAttribute('data-option');
-            const userVoteKey = `user_vote_${cardId}`;
-
-            if (localStorage.getItem(userVoteKey)) {
-                alert('You have already voted for this question.');
-                return;
-            }
-
-            voteCounts[option]++;
-            localStorage.setItem(`votes_${cardId}`, JSON.stringify(voteCounts));
-            localStorage.setItem(userVoteKey, true);
-
-            displayResults();
-        });
+document.querySelectorAll('.vote-option').forEach(optionButton => {
+    optionButton.addEventListener('click', () => {
     });
-
-    function displayResults() {
-        resultsElem.innerHTML = `
-            <h4>Results:</h4>
-            ${Object.entries(options).map(([key, value]) => `
-                <p>${value}: ${voteCounts[key]} votes</p>
-            `).join('')}
-        `;
-    }
-
-    displayResults();
 });
